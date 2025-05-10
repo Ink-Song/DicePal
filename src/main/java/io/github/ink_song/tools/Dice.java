b9593fe2fe0ad75c7babae37b822c76b253012d2
Ink-Song <sunruh@proton.me>
2025-05-11 01:04:52 +0200

    Built out dice class.

--- src/main/java/io/github/ink_song/tools/Dice.java
+++ src/main/java/io/github/ink_song/tools/Dice.java
@@ -1,4 +1,49 @@
 package io.github.ink_song.tools;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Dice {
+  private final List<Die> dice;
+  private int[] rolls;
+  private int total;
+
+  public Dice() {
+    dice = new ArrayList<Die>();
+  }
+
+  public void roll(){
+    rolls = dice.stream().mapToInt(Die::roll).toArray();
+  }
+
+  public void addDice(Die d) {
+    dice.add(d);
+  }
+
+  public void removeDice(Die d) {
+    dice.remove(d);
+  }
+
+  public int getTotal() {
+    total = calculateTotal();
+    return total;
+  }
+
+  public List<Die> getDice() {
+    return dice;
+  }
+
+  public int[] getRolls() {
+    return rolls;
+  }
+
+  public int getAmount() {
+    return dice.size();
+  }
+
+  private int calculateTotal() {
+    return Arrays.stream(rolls).sum();
+  }
 }
--- src/main/java/io/github/ink_song/tools/DiceRoller.java
+++ src/main/java/io/github/ink_song/tools/DiceRoller.java
@@ -3,5 +3,16 @@ package io.github.ink_song.tools;
 
 
 public class DiceRoller {
+  private Dice dice;
+  private int[] rolls;
+  private int total;
 
+  public DiceRoller(Dice dice) {
+    this.dice = dice;
+    rolls = new int[dice.getAmount()];
+  }
+
+  public void roll() {
+
+  }
 }
