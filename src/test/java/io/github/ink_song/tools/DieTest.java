56c0ad0d4cf18831999301b212c88c9e9bd89c0f
Ink-Song <sunruh@proton.me>
2025-05-11 15:18:24 +0200

    Made tests public.

--- src/test/java/io/github/ink_song/tools/DieTest.java
+++ src/test/java/io/github/ink_song/tools/DieTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DieTest {
+public class DieTest {
   private Die firstDie;
   private Die secondDie;
   private List<Die> dice;
