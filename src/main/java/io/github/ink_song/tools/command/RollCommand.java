24b1b8fd3d253d74a26179adbba647999aaa6819
Ink-Song <sunruh@proton.me>
2025-05-12 08:04:42 +0200

    Added comments to rollCommand interface.

--- src/main/java/io/github/ink_song/tools/command/RollCommand.java
+++ src/main/java/io/github/ink_song/tools/command/RollCommand.java
@@ -4,6 +4,12 @@ public class RollCommand implements Command {
   //Format
   // roll 1d20 + 2d10 + 4;
 
+  public RollCommand(String input) {
+    //basically needs a roll parser to read the input and generate dice from it.
+    //need to figure out program structure. Where are the dice objects stored? Do we make new ones each time?
+    //Are we getting an existing set of dice or making one?
+  }
+
   @Override
   public void execute(){
 
