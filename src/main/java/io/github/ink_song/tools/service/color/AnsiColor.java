4b88b9c25fca57d622328aa564cd88201983457a
Ink-Song <sunruh@proton.me>
2025-06-15 00:12:37 +0200

    Fix faulty AnsiColor declarations in Enum.

--- src/main/java/io/github/ink_song/tools/service/color/AnsiColor.java
+++ src/main/java/io/github/ink_song/tools/service/color/AnsiColor.java
@@ -3,11 +3,11 @@ package io.github.ink_song.tools.service.color;
 public enum AnsiColor {
   BLACK("\u001B[30m"),
   RED("\u001B[31m"),
-  YELLOW("\u001B[32m"),
-  GREEN("\u001B[33m"),
-  CYAN("\u001B[34m"),
-  BLUE("\u001B[35m"),
-  PURPLE("\u001B[36m"),
+  YELLOW("\u001B[33m"),
+  GREEN("\u001B[32m"),
+  CYAN("\u001B[36m"),
+  BLUE("\u001B[34m"),
+  PURPLE("\u001B[35m"),
   WHITE("\u001B[37m"),
   RESET("\u001B[0m");
 
