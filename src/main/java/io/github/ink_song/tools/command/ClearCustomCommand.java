104ed1fbf3530b9e2d904fe09d7c55a92be8aaa0
Ink-Song <sunruh@proton.me>
2025-06-18 22:30:36 +0200

    Add color to ClearCustomCommand prompt.

--- src/main/java/io/github/ink_song/tools/command/ClearCustomCommand.java
+++ src/main/java/io/github/ink_song/tools/command/ClearCustomCommand.java
@@ -3,20 +3,26 @@ package io.github.ink_song.tools.command;
 import io.github.ink_song.tools.model.CommandResult;
 import io.github.ink_song.tools.service.CustomCommandRegistry;
 import io.github.ink_song.tools.service.UserPrompter;
+import io.github.ink_song.tools.service.color.AnsiColor;
+import io.github.ink_song.tools.util.StringUtil;
 
 import java.io.IOException;
-import java.security.InvalidParameterException;
 import java.util.Scanner;
 
 public class ClearCustomCommand implements Command {
   private final CustomCommandRegistry customCommandRegistry;
   private final Scanner scanner;
   private final UserPrompter userPrompter = new UserPrompter();
-  private final String question = "Clear all custom commands? This action cannot be undone.";
+  private final String question;
 
   public ClearCustomCommand(CustomCommandRegistry customCommandRegistry, Scanner scanner) {
     this.customCommandRegistry = customCommandRegistry;
     this.scanner = scanner;
+
+    String start = "Clear all custom commands? ";
+    String end = "This action cannot be undone.";
+
+    question = start + StringUtil.color(end, AnsiColor.RED);
   }
 
   /**
@@ -28,7 +34,7 @@ public class ClearCustomCommand implements Command {
   public CommandResult execute(String[] args) {
     CommandResult commandResult = null;
     if (!userPrompter.isConfirmed(scanner, customCommandRegistry, question)){
-      commandResult = new CommandResult(false, "Custom Command Registry wipe cancelled.");
+      commandResult = new CommandResult(false, "Registry Wipe Cancelled.");
       return commandResult;
     }
 
